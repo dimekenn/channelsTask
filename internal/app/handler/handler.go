2d35301d126de56886703ccfc80f6fdd3fe8de98
dimekenn <dimeknn@gmail.com>
2022-01-10 12:41:40 +0600

    add readme

--- internal/app/handler/handler.go
+++ internal/app/handler/handler.go
@@ -3,6 +3,7 @@ package handler
 import (
 	"chansTask/internal/app/service"
 	"github.com/labstack/echo/v4"
+	"github.com/labstack/gommon/log"
 	"net/http"
 	"strconv"
 )
@@ -16,11 +17,19 @@ func NewHandler(service service.Service) *Handler {
 }
 
 func (h *Handler) FromNChannelsToOneChannel(c echo.Context) error {
-	x := c.QueryParam("x")
-	y := c.QueryParam("y")
-	xx, _ := strconv.Atoi(x)
-	yy, _ := strconv.Atoi(y)
-	res := h.service.FromNChannelsToOneChannel(xx, yy)
+	ch := c.QueryParam("channels")
+	ran := c.QueryParam("randomin")
+	channelsCount, chErr := strconv.Atoi(ch)
+	if chErr!=nil{
+		log.Infof("bad request")
+		return echo.NewHTTPError(http.StatusBadRequest, "cannot parse param")
+	}
+	randomIn, ranErr := strconv.Atoi(ran)
+	if ranErr!=nil{
+		log.Infof("bad request")
+		return echo.NewHTTPError(http.StatusBadRequest, "cannot parse param")
+	}
+	res := h.service.FromNChannelsToOneChannel(channelsCount, randomIn)
 	return c.JSON(http.StatusOK, res)
 }
 
